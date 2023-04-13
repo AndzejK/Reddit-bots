48393120fdea0fb247e17e924fb953ac9a495c7c
Andzej Krupoves <andrejkrupovec@gmail.com>
2023-04-13 10:14:56 +0300

    getting the new topics/subreddits based on time, 24hours, saving these topics into a file on a MAC

---
+++ getting_subreddits.py
@@ -0,0 +1,30 @@
+import praw 
+import os
+from datetime import datetime,timedelta
+# Instance
+reddit = praw.Reddit(
+    # read-only
+    client_id=os.getenv("clientIDreddit"), # clientIDreddit
+    client_secret=os.getenv("clientSecretReddit"), #clientSecretReddit
+    user_agent="Andrey",
+    # write and read
+    password=os.getenv("passwordReddit"), #passwordReddit
+    username="krupoves",
+)
+# Getting info abount community dedicated to a specific topic or theme - SUBREDDIT
+subreddit = reddit.subreddit("sydney") #r/sydney
+
+with open('reddit_last_24_topics_on_Sydney.txt','w') as file:
+    for index,topic in enumerate(subreddit.new()):
+        current_time=datetime.utcnow() 
+        topic_time=datetime.utcfromtimestamp(topic.created) # coverted from sec to 2023-04-13 01:35:59 format
+        # print(current_time,topic_time) #2023-04-13 06:44:54.634701 1681171570.0
+        delta_time=current_time-topic_time
+        # print(delta_time)
+        if delta_time<=timedelta(hours=24):
+            if topic.selftext=='':
+                content='\tNo content just title...'
+                file.write(f"No.{index}\n{topic.title}\n{content}\nPosted: {topic_time}\n\n")
+            else:
+                file.write(f"No.{index}\n{topic.title}\n{topic.selftext}\nPosted: {topic_time}\n\n")   
+            #print(f"No.{index}\n{topic.title}\n{topic.selftext}\nPosted:{topic_time}\n\n")
